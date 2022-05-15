a574435ec5aa3253aedd34fa7fca3557fe3ef4bf
ogg <dromadairefever@gmail.com>
2022-05-15 20:20:43 +0200

    fix typo in question

--- gennaker/src/Components/Evaluation.js
+++ gennaker/src/Components/Evaluation.js
@@ -5,8 +5,8 @@ function Evaluation (props) {
     
     return (
         <div>
-            <Question filePath="questions/meteo/Evolution_vent_sous_grain.md" />
-            <Question filePath="questions/meteo/incontournables/etablissement_brise_thermique.md" />
+            <Question filePath="questions/Meteo/Evolution_vent_sous_grain.md" />
+            <Question filePath="questions/Meteo/incontournables/etablissement_brise_thermique.md" />
         </div>
     )
 }
