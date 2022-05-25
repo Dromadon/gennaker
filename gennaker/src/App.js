faf61c97de449fd84d4c54ce6561daf4cb6ac769
ogg <dromadairefever@gmail.com>
2022-05-25 19:30:37 +0200

    preparation for future work

--- gennaker/src/App.js
+++ gennaker/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
       <div className="App">
-        <Evaluation />
+        <Evaluation support="catamaran" evalStructure="evaluations/catamaran.json"/>
       </div>
       <div>
         <h3>Made with love</h3>
--- gennaker/src/Components/Evaluation.js
+++ gennaker/src/Components/Evaluation.js
@@ -4,14 +4,15 @@ import { EvaluationGenerator } from '../Logic/EvaluationGenerator'
 
 function Evaluation (props) {
     
-    // Generate eval with parameters
+    // Generate eval with parameters -> in constructor ?
 
     // Display with loop over questions (and/or categories)
+    // for each question, build filePath with category, section, and fileName
 
     return (
         <div>
             <h2>Evaluation</h2>
-            <Question filePath="questions/meteo/Evolution_vent_sous_grain.md" />
+            <Question filePath="questions/meteo/phenomene_local/evolution_vent_sous_grain.md" />
             <Question filePath="questions/meteo/incontournables/etablissement_brise_thermique.md" />
         </div>
     )
