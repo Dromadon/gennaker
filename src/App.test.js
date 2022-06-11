68a64260f758d32c031b46c4f2e91a5de972e5d4
ogg <dromadairefever@gmail.com>
2022-06-11 23:48:51 +0200

    remove unused test

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const credits = screen.getByText(/love/i);
-  expect(credits).toBeInTheDocument();
-});
