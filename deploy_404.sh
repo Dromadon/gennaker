01c6f9bcb25f06f6703ffd9bd47701cda2b2d46a
ogg <dromadairefever@gmail.com>
2022-06-27 23:15:32 +0200

    adds deriveur eval structure

--- deploy_404.sh
+++ deploy_404.sh
@@ -3,6 +3,7 @@ set -e
 
 git switch gh-pages;
 git fetch;
+git reset --hard origin/gh-pages;
 git restore --source master -- src/404.html;
 mv src/404.html . && rm -r src/;
 git add 404.html && git commit --amend --no-edit && git push --force-with-lease;
