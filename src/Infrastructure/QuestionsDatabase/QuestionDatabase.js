12c56668ae676435550c2ff9538531bac2383e78
ogg <dromadairefever@gmail.com>
2023-01-06 19:01:11 +0100

    linting

--- src/Infrastructure/QuestionsDatabase/QuestionDatabase.js
+++ src/Infrastructure/QuestionsDatabase/QuestionDatabase.js
@@ -4,37 +4,35 @@ const _ = require('lodash')
 
 class QuestionDatabase {
 
-    async getQuestions({category, section, number, support, excludedQuestions}={}){   
+    async getQuestions({ category, section, number, support, excludedQuestions } = {}) {
         if (category === undefined || category === null)
             throw new Error("Category is undefined or null");
         if (section === undefined || section === null)
             throw new Error("Section is undefined or null");
 
-        console.log("Getting "+number+" questions from DB for category "+category+" and section "+section+" with support "+support)
+        console.log("Getting " + number + " questions from DB for category " + category + " and section " + section + " with support " + support)
 
-        const questionsData = await this.fetchQuestionDatabaseFile({category: category, section: section});
-        
+        const questionsData = await this.fetchQuestionDatabaseFile({ category: category, section: section });
+
         const filteredQuestionsData = questionsData
             .filter(questionData => this.filterSupport(questionData, support))
             .filter(questionData => this.filterExcludedQuestions(questionData, excludedQuestions))
-        
-        const questions = this.getRandomQuestions(filteredQuestionsData, number).map((question) => {    
+
+        const questions = this.getRandomQuestions(filteredQuestionsData, number).map((question) => {
             return new Question({
-            fileName: category+"/"+section+"/"+question.fileName,
-            answerSize: question.answerSize,
-            supports: question.supports
+                fileName: category + "/" + section + "/" + question.fileName,
+                answerSize: question.answerSize,
+                supports: question.supports
             })
         })
-        
-        console.debug("All questions fetched from remote DB are ");
-        console.debug(questions);
+
         return questions
-        
-        
+
+
     }
 
     filterSupport(questionData, support) {
-        if(support === undefined)
+        if (support === undefined)
             return true
         else {
             return !questionData.hasOwnProperty("supports") || questionData["supports"].includes(support)
@@ -42,7 +40,7 @@ class QuestionDatabase {
     }
 
     filterExcludedQuestions(questionData, excludedQuestions) {
-        if(excludedQuestions === undefined)
+        if (excludedQuestions === undefined)
             return true
         else {
             if (excludedQuestions.some(excludedQuestion => excludedQuestion.fileName.split('/').pop() === questionData.fileName))
@@ -53,17 +51,17 @@ class QuestionDatabase {
     }
 
     getRandomQuestions(questionsData, number) {
-        if(number === undefined)
+        if (number === undefined)
             return questionsData
         else
             return _.sampleSize(questionsData, number);
     }
 
-    async fetchQuestionDatabaseFile({category, section}={}) {
-        console.debug("Fetching questions for category "+category+" and section "+section);
-        const questionDB = await (await fetch(process.env.PUBLIC_URL + "/questions/" + category + "/" + section + "/db.json", 
-            { 
-                headers : { 
+    async fetchQuestionDatabaseFile({ category, section } = {}) {
+        console.debug("Fetching questions for category " + category + " and section " + section);
+        const questionDB = await (await fetch(process.env.PUBLIC_URL + "/questions/" + category + "/" + section + "/db.json",
+            {
+                headers: {
                     'Content-Type': 'application/json',
                     'Accept': 'application/json'
                 }
@@ -73,4 +71,4 @@ class QuestionDatabase {
     }
 }
 
-export {QuestionDatabase}
+export { QuestionDatabase }
